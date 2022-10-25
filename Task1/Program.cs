c18c015b29ed1f9d2197b0b4e933491e578c3cd7
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-25 12:26:23 +0300

    Программа, которая принимает на вход координаты точки (X и Y),

---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Программа, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+bool ValidateCoord(int x, int y)
+{
+    if (x == 0 || y == 0)
+    {
+        Console.WriteLine("the point is on one of the coordinate lines!");
+        return false;
+    }
+    return true;
+}
+
+int GetQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    return 4;
+    
+}
+
+
+int x = Prompt ("Enter x: ");
+int y = Prompt ("Enter y: ");
+if (ValidateCoord(x, y))
+{
+    int quarter = GetQuarter(x, y);
+    Console.WriteLine($"quarter number = {quarter}:");
+}
+
+
+
