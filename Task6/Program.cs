abf20e2c62561b6651758cb54b56d589268c54a8
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-27 14:07:31 +0300

    Напишите программу, которая генерирует последовательность случайных чисел до тех пор,

---
+++ Task6/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая генерирует последовательность случайных чисел до тех пор,
+// пока не сгенерирует кратное число введенному в начале.
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+
+
+void Starter()
+{
+    int userAnswer = Prompt("Enter digit from 1 to 10: ");
+    
+    while (true)
+    {
+        int num = new Random().Next(1, 11);
+        if (num % userAnswer == 0)
+        {
+            Console.WriteLine(num);
+            break;
+        }
+        Console. WriteLine($"until it worked out {num}");
+    }
+}
+
+
+Starter();
+
+
