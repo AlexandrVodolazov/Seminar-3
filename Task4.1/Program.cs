a2d538da9f3c0a4edf03915a45a5cea7f30c95dc
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-27 12:27:32 +0300

    Программа, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.

---
+++ Task4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int Prompt(string message) // Функция ввода числа с консоли.
+{
+    Console.Write(message);                  // Выводит сообщение в консоли.
+    return int.Parse(Console.ReadLine());    //Считать строку с консоли и преобразовать в целое значение.
+}
+
+int N = Prompt("Enter a number greater 0: ");
+
+int count = 1;                                    // Счетчик.
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+}
+
+
